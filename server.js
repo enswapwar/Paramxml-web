1c549c81af856a048a3fb08a9df5682294fc6728
enswapwar <115549512+enswapwar@users.noreply.github.com>
2026-06-23 11:21:41 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
+const http = require("http");
 const { execFile } = require("child_process");
 
-execFile(
-    "./ParamXML",
-    ["--help"],
-    (err, stdout, stderr) => {
-        console.log(stdout);
-        console.log(stderr);
-    }
-);
+const server = http.createServer((req, res) => {
+    execFile("./ParamXML", [], (err, stdout, stderr) => {
+        res.writeHead(200, {
+            "Content-Type": "text/plain; charset=utf-8"
+        });
+
+        if (err) {
+            res.end(String(err) + "\n" + stderr);
+            return;
+        }
+
+        res.end(stdout || stderr);
+    });
+});
+
+server.listen(process.env.PORT || 3000);
